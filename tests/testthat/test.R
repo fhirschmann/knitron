a3ae8fb9728bd5052f55e913d09981fa05907698
Fabian Hirschmann <fabian@hirschmann.email>
2015-01-26 18:45:40 +0100

    add tests for latex

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,4 +1,4 @@
-run_knit <- function(code, echo = FALSE, strip = TRUE, ...) {
+run_knit <- function(code, echo = FALSE, strip = TRUE, latex = FALSE, ...) {
   tmp <- tempfile(pattern = "knitron.test.")
   dir.create(tmp)
   opts_knit$set(base.dir = tmp)
@@ -14,7 +14,10 @@ run_knit <- function(code, echo = FALSE, strip = TRUE, ...) {
                                                 paste("'", x, "'", sep=""), x)),
                       sep = "=", collapse = ","), sep = "")
 
-  text <- paste("```{r, engine = 'ipython'", args, "}\n", code, "\n```", sep="")
+  text <- if (latex)
+    paste("<<engine = 'ipython'", args, ">>=\n", code, "\n", "@", sep="")
+  else
+    paste("```{r, engine = 'ipython'", args, "}\n", code, "\n```", sep="")
 
   # Set quiet to FALSE when something goes wrong
   out <- knit(text=text, quiet = TRUE)
@@ -26,41 +29,55 @@ run_knit <- function(code, echo = FALSE, strip = TRUE, ...) {
     list(out = out, files = files)
 }
 
-test_that("Matplotlib is not loaded", {
+test_that("[markdown] Matplotlib is not loaded", {
   expect_equal(run_knit("import sys; 'matplotlib' in sys.modules", knitron.matplotlib = FALSE)$out, "False")
 })
 
-test_that("Matplotlib is loaded", {
+test_that("[markdown] Matplotlib is loaded", {
   expect_equal(run_knit("import sys; 'matplotlib' in sys.modules")$out, "True")
 })
 
-test_that("Waiting for code exeuction", {
+test_that("[markdown] Waiting for code exeuction", {
   expect_equal(run_knit("from time import sleep; sleep(4); 4")$out, "4")
 })
 
-test_that("Implicit printing: automatic", {
+test_that("[markdown] Implicit printing: automatic", {
   expect_equal(run_knit(4, echo = TRUE), list(out = "python\n4\n4", files = character(0)))
   expect_equal(run_knit(4, echo = FALSE), list(out = "4", files = character(0)))
 })
 
-test_that("Implicit printing: off", {
+test_that("[markdown] Implicit printing: off", {
   expect_equal(run_knit(4, echo = FALSE, knitron.autoprint = FALSE), list(out = "4", files = character(0)))
 })
 
-test_that("Empty input outputs empty output", {
+test_that("[markdown] Empty input outputs empty output", {
   expect_equal(run_knit(""), list(out = "", files = character(0)))
 })
 
-test_that("Plot is created", {
+test_that("[markdown] Plot is created", {
   res <- run_knit("plt.plot([1, 2, 3])")
   expect_equal(res$out, "![plot of chunk unnamed-chunk-1](figure/unnamed-chunk-1-1.png) ")
   expect_equal(res$files, "figure/unnamed-chunk-1-1.png")
 })
 
-test_that("Two plots are created", {
+test_that("[latex] Plot is created", {
+  res <- run_knit("plt.plot([1, 2, 3])", latex = TRUE)
+  expect_true(grepl("includegraphics.*figure/unnamed-chunk-1-1", res$out))
+  expect_equal(res$files, "figure/unnamed-chunk-1-1.pdf")
+})
+
+test_that("[markdown] Two plots are created", {
   res <- run_knit(paste("x = plt.figure(); x1 = x.add_subplot(111); x1.plot([1, 2, 3])",
                     "y = plt.figure(); y1 = y.add_subplot(111); y1.plot([5, 6])", sep="\n"))
   expect_equal(res$out, paste("![plot of chunk unnamed-chunk-1](figure/unnamed-chunk-1-1.png)",
                               "\n![plot of chunk unnamed-chunk-1](figure/unnamed-chunk-1-2.png) "))
   expect_equal(res$files, c("figure/unnamed-chunk-1-1.png", "figure/unnamed-chunk-1-2.png"))
+})
+
+test_that("[latex] Two plots are created", {
+  res <- run_knit(paste("x = plt.figure(); x1 = x.add_subplot(111); x1.plot([1, 2, 3])",
+                        "y = plt.figure(); y1 = y.add_subplot(111); y1.plot([5, 6])", sep="\n"),
+                  latex = TRUE)
+  expect_true(grepl("includegraphics.*figure/unnamed-chunk-1-1.*includegraphics.*figure/unnamed-chunk-1-2", res$out))
+  expect_equal(res$files, c("figure/unnamed-chunk-1-1.pdf", "figure/unnamed-chunk-1-2.pdf"))
 })
